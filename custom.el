fefa17504dcab903c92a4517097f79fd9fb15b6b
benwiz <bwisialowski@gmail.com>
2019-12-29 21:30:59 -0800

    more progress

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit use-package))))
+ '(package-selected-packages
+   (quote
+    (gnu-elpa-keyring-update queue-0\.2 ob-clojure cider clojure-mode flycheck-clj-kondo paredit flycheck magit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
