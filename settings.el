521c4dda697b770101f93b11a08cb0c4a150f0ac
benwiz <bwisialowski@gmail.com>
2019-12-31 10:14:36 -0800

    git gutter

--- settings.el
+++ settings.el
@@ -126,8 +126,27 @@
 (global-set-key (kbd "C-c i") 'load-init-el)
 
 (use-package magit
+   :config
+   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+(use-package git-gutter
+  :diminish git-gutter-mode
+  :config (global-git-gutter-mode)
+  :init
+  (progn
+    (setq git-gutter:separator-sign " "
+          git-gutter:lighter " GG"))
   :config
-  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+  (progn
+    (set-face-background 'git-gutter:deleted "#990A1B")
+    (set-face-foreground 'git-gutter:deleted "#990A1B")
+    (set-face-background 'git-gutter:modified "#00736F")
+    (set-face-foreground 'git-gutter:modified "#00736F")
+    (set-face-background 'git-gutter:added "#546E00")
+    (set-face-foreground 'git-gutter:added "#546E00"))
+  :bind (("C-x p" . git-gutter:previous-hunk)
+         ("C-x n" . git-gutter:next-hunk)
+         ("C-x v =" . git-gutter:popup-hunk)
+         ("C-x v r" . git-gutter:revert-hunk)))
 
 (use-package ivy
   :config
